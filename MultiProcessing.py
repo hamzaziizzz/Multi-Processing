a4f51e5f5aa9d9d5b31022f041d58ad814d68e14
Hamza Aziz <hamzaaziz822@gmail.com>
2023-07-19 15:01:51 +0530

    Limit Memory and Distribute process on most optimal core

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -1,24 +1,59 @@
 import multiprocessing
 import psutil
+# import time
 
 from MultipleIPCameras import create_thread
 from LiveStreaming import read_frames
 from SharedBuffer import SHARED_BUFFER
 
 if __name__ == "__main__":
+    # Get CPU usage per core
+    cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
+    print(f"CPU Usage: {cpu_percent}")
+    # cpu_percent = None
+    # for i in range(10):
+    #     cpu_percent = psutil.cpu_percent(interval=1, percpu=True)
+    #     print(f"CPU Usage: {cpu_percent}")
+
+    # Sort the CPU usage per core in ascending order
+    sorted_cores = sorted(enumerate(cpu_percent), key=lambda x: x[1])
+
+    # Select the two cores with the minimum usage
+    min_usage_cores = sorted_cores[:2]
+    print(min_usage_cores)
+
+    # Select CPU core for the producer process
+    producer_core = min_usage_cores[0][0]
+    print(f"Producer Core: {producer_core}")
+
+    # Select CPU core for the consumer process
+    consumer_core = min_usage_cores[1][0]
+    print(f"Consumer Core: {consumer_core}")
+
     producer_process = multiprocessing.Process(target=create_thread, args=(SHARED_BUFFER, ))
     consumer_process = multiprocessing.Process(target=read_frames, args=(SHARED_BUFFER, ))
 
-    # Set the CPU affinity for each process
-    producer_affinity = psutil.Process(producer_process.pid)
-    producer_affinity.cpu_affinity([1])  # Set process 1 to run on CPU core 1
-
-    consumer_affinity = psutil.Process(consumer_process.pid)
-    consumer_affinity.cpu_affinity([2, 3, 4])  # Set process 2 to run on CPU core 2, core 3 and core 4
-    
     # Start the processes
     producer_process.start()
+    # time.sleep(60)
     consumer_process.start()
+
+    # Set the CPU affinity for each process
+    producer_affinity = psutil.Process(producer_process.pid)  # .cpu_affinity([producer_core])
+    producer_affinity.cpu_affinity([producer_core])
+    print(f"CPU Affinity for Producer Process: {producer_affinity.cpu_affinity()}")
+    consumer_affinity = psutil.Process(consumer_process.pid)  # .cpu_affinity([consumer_core])
+    consumer_affinity.cpu_affinity([consumer_core])
+    print(f"CPU Affinity for Consumer Process: {consumer_affinity.cpu_affinity()}")
+
+    cpu_percent = psutil.cpu_percent(interval=10, percpu=True)
+    print(f"CPU Usage: {cpu_percent}")
+    for i in range(10):
+        cpu_percent = psutil.cpu_percent(interval=10, percpu=True)
+        print(f"CPU Usage: {cpu_percent}")
+
+    print(f"CPU Affinity for Producer Process: {producer_affinity.cpu_affinity()}")
+    print(f"CPU Affinity for Consumer Process: {consumer_affinity.cpu_affinity()}")
 
     # Wait for both processes to finish
     producer_process.join()
--- MultipleIPCameras.py
+++ MultipleIPCameras.py
@@ -3,13 +3,15 @@ from threading import Thread
 
 # Define the camera IP addresses
 camera_ips = [
-    "http://192.168.17.158:4747/video",
-    "http://192.168.18.197:4747/video",
-    "http://192.168.16.232:4747/video",
-    "http://192.168.3.223:4747/video",
-#     "http://192.168.12.10:4747/video",
-    "http://192.168.18.61:4747/video"
+    "http://192.168.17.158:4747/video"
+    # "http://192.168.18.197:4747/video",
+    # "http://192.168.16.232:4747/video",
+    # "http://192.168.3.223:4747/video",
+    # "http://192.168.12.10:4747/video"
+    # "http://192.168.18.61:4747/video"
 ]
+
+MAXIMUM_SIZE = 256
 
 
 # Define a class to handle camera streaming in a separate thread
@@ -24,31 +26,48 @@ class CameraStream(Thread):
         capture = cv2.VideoCapture(self.camera_ip)
 
         # Initialize variables for FPS calculation
-        frame_count = 0
-        start_time = cv2.getTickCount()
+        # frame_count = 0
+        # start_time = cv2.getTickCount()
 
         while True:
             success, frame = capture.read()
             if success is False:
                 break
 
-            # Increment the frame number as a frame is being read by the program
-            frame_count += 1
-            # Calculate elapsed time
-            elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+            # Resize the frame
+            frame = cv2.resize(frame, (1280, 768))
 
-            # Check if one second has elapsed
-            if elapsed_time >= 1.0:
-                # Calculate FPS
-                fps = f"FPS: {(frame_count / elapsed_time):.2f}"
-                print(fps)
-                # Reset variables for the next calculation
-                frame_count = 0
-                start_time = cv2.getTickCount()
+            # Put some random text on the frame
+            frame = cv2.putText(frame, "Hamza Aziz", (70, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+
+            # fact = 1
+            # for i in range(1, 501):
+            #     fact *= i
+            # print(fact)
+
+            # # Increment the frame number as a frame is being read by the program
+            # frame_count += 1
+            # # Calculate elapsed time
+            # elapsed_time = (cv2.getTickCount() - start_time) / cv2.getTickFrequency()
+            #
+            # # Check if one second has elapsed
+            # if elapsed_time >= 1.0:
+            #     # Calculate FPS
+            #     # fps = f"FPS: {(frame_count / elapsed_time):.2f}"
+            #     # print(fps)
+            #     # Reset variables for the next calculation
+            #     frame_count = 0
+            #     start_time = cv2.getTickCount()
 
             # Process the frame as needed (e.g. display, save, etc.)
-            self.shared_buffer.put(frame)
-            
+            if self.shared_buffer.qsize() < MAXIMUM_SIZE:
+                print(f"Current Queue Size: {self.shared_buffer.qsize()}")
+                self.shared_buffer.put(frame)
+            else:
+                while not self.shared_buffer.empty():
+                    self.shared_buffer.get()
+                # raise ValueError("Queue has reached its maximum size.")
+
             # Exit the program if 'q' is pressed
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -58,6 +77,7 @@ class CameraStream(Thread):
 
 
 def create_thread(shared_buffer):
+    print("Process 1 Started")
     # Create a start a separate thread for each camera
     for ip in camera_ips:
         thread = CameraStream(ip, shared_buffer)
